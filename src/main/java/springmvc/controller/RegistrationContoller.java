eb323265ca890e09c698ecb01180e3f76859b379
Harsh Rathore <rathorehrharsh5@gmail.com>
2026-06-26 13:08:50 +0530

    Connect our project spring mvc with spring orm using following steps

--- src/main/java/springmvc/controller/RegistrationContoller.java
+++ src/main/java/springmvc/controller/RegistrationContoller.java
@@ -1,15 +1,20 @@
 package springmvc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import springmvc.model.User;
+import springmvc.service.UserService;
 
 @Controller
 public class RegistrationContoller {
-
+	
+	@Autowired
+	private UserService userService;
+	
 //	Outside @ModelAttributex
 	@ModelAttribute
 	public void commonData(Model m) {
@@ -22,34 +27,6 @@ public class RegistrationContoller {
 		return "userRegistration";
 	}
 
-//	This is the part of normal @RequestParam
-//	@RequestMapping("/processform")
-//	public String save(@RequestParam(name="email",required=true) String usere, @RequestParam(name="name") String usern, @RequestParam(name="password") String userp,Model model) {
-//		
-//		System.out.println(usere + " " + usern + " " + userp);
-//		
-//		model.addAttribute("useremail", usere);
-//		model.addAttribute("username", usern);
-//		model.addAttribute("userpassword", userp);
-//		return "success";
-//	}
-
-//	Now we are using easiest way
-//	@RequestMapping("/processform")
-//	public String userHandler(@RequestParam(name = "email") String usere, @RequestParam(name = "name") String usern,
-//			@RequestParam(name = "password") String userp, Model model) {
-//
-//		User user = new User();
-//		user.setEmail(usere);
-//		user.setName(usern);
-//		user.setPassword(userp);
-//
-//		System.out.println("User: " + user);
-//
-//		model.addAttribute(user);
-//
-//		return "success";
-	
 //	Now we are MODELATTRIBUTE
 	@RequestMapping("/processform")
 	public String userHandler(@ModelAttribute User user,Model model) {
@@ -57,7 +34,8 @@ public class RegistrationContoller {
 		System.out.println("User: " + user);
 
 		model.addAttribute(user);
-
+		int createduser = this.userService.createUser(user);
+		model.addAttribute("msg", "Data is saved for : "+createduser);
 		return "success";
 	}
 }
